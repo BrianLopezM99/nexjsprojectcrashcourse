ca5a39390f50587211c0fe2e10c4647655e482e4
brian-lopez-digital <brian.lopez@digitalvaultstudio.com>
2024-12-03 01:58:01 -0600

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,10 @@
+import Navbar from "../../components/Navbar";
+
+const Layout = ({ children} : {children: React.ReactNode}) => {
+    return ( <main className="font-work-sans">
+        <Navbar />
+        {children}
+    </main> );
+}
+ 
+export default Layout;
